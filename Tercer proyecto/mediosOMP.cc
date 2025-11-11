6a7711fe42a1edd60e49d5b2265ba374c914dabd
juanselospi <juanselospi@gmail.com>
2025-11-11 02:43:50 -0600

    Tercer Proyecto avance 2

--- Tercer proyecto/mediosOMP.cc
+++ Tercer proyecto/mediosOMP.cc
@@ -12,9 +12,13 @@
 #include <cstdio>
 #include "VectorPuntos.h"
 #include <cstring>
+#include <omp.h>
+#include <ctime>
 
+#define HILOS 4
 #define PUNTOS 100000
 #define CLASES 17
+#define MODO 0
 
 
 int totalCambios = 0;	// Contabiliza la totalidad de los cambios realizados al grupo de puntos
@@ -25,21 +29,23 @@ int totalCambios = 0;	// Contabiliza la totalidad de los cambios realizados al g
  *  Utiliza el vector de clases para realizar la asignación
  *  
 **/
-void asignarPuntosAClases( long * clases, int modo ) {  // CAMBIOSSSSSSSSSSS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!
+void asignarPuntosAClases( long * clases, int modo, long muestras, long casillas, int hilos ) {
    long clase, pto;
 
    switch ( modo ) {
       case 0:	// Aleatorio
-         for ( pto = 0; pto < PUNTOS; pto++ ) {
-            clase = rand() % CLASES;
+         #pragma omp parallel for num_threads( hilos )
+         for ( pto = 0; pto < muestras; pto++ ) {
+            unsigned int seed = omp_get_thread_num() + time( NULL );
+            clase = rand_r( &seed ) % casillas;
             clases[ pto ] = clase;
          }
          break;
       case 1:	// A construir por los estudiantes
-
+         #pragma omp parallel for num_threads( hilos )
          // round robin (los puntos se van asignando a los centros uno por uno como cartas repartidas en una mesa de poker)
-         for( pto = 0; pto < PUNTOS; pto++ ) {
-            clases[ pto ] = pto % CLASES;
+         for( pto = 0; pto < muestras; pto++ ) {
+            clases[ pto ] = pto % casillas;
          }
 
          break;
@@ -51,36 +57,47 @@ void asignarPuntosAClases( long * clases, int modo ) {  // CAMBIOSSSSSSSSSSS!!!!
  *  Recibe los centros, puntos y sus contadores, los reinicia, luego los suma de nuevo y promedia actualizando los centros
  *  
 **/
-void actualizarCentros( VectorPuntos * centros, VectorPuntos * puntos, long * clases, long * contClases ) {
+void actualizarCentros( VectorPuntos * centros, VectorPuntos * puntos, long * clases, long * contClases, int hilos ) {
    long clase, pto;
 
-   // primero reinicio los centros y los contadores
-   for( clase = 0; clase < centros->demeTamano(); clase++ ) {
-      ( *centros )[ clase ]->ponga( 0, 0, 0 ); // para los ejes x, y, z
-      contClases[ clase ] = 0;
-   }
+   #pragma omp parallel num_threads( hilos )
+   {
+        #pragma omp for
+        // primero reinicio los centros y los contadores
+        for( clase = 0; clase < centros->demeTamano(); clase++ ) {
+            ( *centros )[ clase ]->ponga( 0, 0, 0 ); // para los ejes x, y, z
+            contClases[ clase ] = 0;
+        }
 
-   // sumar puntos de cada clase
-   for( pto = 0; pto < puntos->demeTamano(); pto++ ) {
-      clase = clases[ pto ];
-      ( *centros )[ clase ]->sume( ( * puntos )[ pto ] );
-      contClases[ clase ]++;
-   }
+        #pragma omp for
+        // sumar puntos de cada clase
+        for( pto = 0; pto < puntos->demeTamano(); pto++ ) {
+            clase = clases[ pto ];
 
-   // calcula el promedio dividiendo la suma de puntos entre su total
-   for( clase = 0; clase < centros->demeTamano(); clase++ ) {
+            #pragma omp critical
+            {
+            ( *centros )[ clase ]->sume( ( * puntos )[ pto ] );
+            contClases[ clase ]++;
+            }
+        }
 
-      // si existen clases promedio
-      if( contClases[ clase ] > 0) {
-         ( *centros )[ clase ]->divida( contClases[ clase ] );
-      }
+        #pragma omp for
+        // calcula el promedio dividiendo la suma de puntos entre su total
+        for( clase = 0; clase < centros->demeTamano(); clase++ ) {
+
+            // si existen clases promedio
+            if( contClases[ clase ] > 0) {
+                ( *centros )[ clase ]->divida( contClases[ clase ] );
+            }
+        }
    }
 }
 
 
-void actualizarPuntos( VectorPuntos * centros, VectorPuntos * puntos, long * clases, long * contClases, long &cambios ) {
+void actualizarPuntos( VectorPuntos * centros, VectorPuntos * puntos, long * clases, long * contClases, long &cambios, int hilos ) {
    long pto, actual, nuevo;
 
+   #pragma omp parallel for num_threads( hilos ) reduction( +:cambios )
    // verifico si para cada punto...
    for( pto = 0; pto < puntos->demeTamano(); pto++ ) {
       actual = clases[ pto ];
@@ -88,8 +105,8 @@ void actualizarPuntos( VectorPuntos * centros, VectorPuntos * puntos, long * cla
 
       // si su centro actual permanece como el mas cerano
       if( actual != nuevo ) {
-         clases[ pto ] = nuevo;
-         cambios++;
+        clases[ pto ] = nuevo;
+        cambios++;
       }
    }
 }
@@ -106,10 +123,23 @@ int main( int cantidad, char ** parametros ) {
    long casillas = CLASES;
    long muestras = PUNTOS;
    const char *nombreArchivo = "ci0117.eps";
+   double start, finish, wused; // para tomar los tiempos
+   int hilos = HILOS;
+   int modo = MODO;
 
    // input del usuario con validacion de entrada
-   if( cantidad > 1 ) { // para los puntos
-      muestras = atol( parametros[ 1 ] );
+
+   if( cantidad > 1 ) { // numero de hilos
+      hilos = atol( parametros[ 1 ] );
+
+      if( hilos < 1 ) {
+        hilos = HILOS;
+        printf( "Cantidad de hilos invalida, usando valor por defecto %d\n", hilos );
+      }
+   }
+
+   if( cantidad > 2 ) { // para los puntos
+      muestras = atol( parametros[ 2 ] );
 
       if( muestras < 1 ) {
          muestras = PUNTOS;
@@ -117,8 +147,8 @@ int main( int cantidad, char ** parametros ) {
       }
    }
 
-   if( cantidad > 2 ) { // para las clases
-      casillas = atol( parametros[ 2 ] );
+   if( cantidad > 3 ) { // para las clases
+      casillas = atol( parametros[ 3 ] );
 
       if( casillas < 1 ) {
          casillas = CLASES;
@@ -126,8 +156,8 @@ int main( int cantidad, char ** parametros ) {
       }
    }
 
-   if( cantidad > 3 ) { // si el usuario le quiere dar un nombre al archivo ( nombres diferentes permiten comparar corridas con diversos parametros de entrada visualmente )
-      nombreArchivo = parametros[ 3 ];
+   if( cantidad > 4 ) { // si el usuario le quiere dar un nombre al archivo ( nombres diferentes permiten comparar corridas con diversos parametros de entrada visualmente )
+      nombreArchivo = parametros[ 4 ];
 
       // comprobar extension .eps correcta
       const char *extencion = strrchr( nombreArchivo, '.' );
@@ -139,7 +169,18 @@ int main( int cantidad, char ** parametros ) {
       }
    }
 
-   printf( "Generando %ld puntos, para %ld clases -> salida: %s\n", muestras, casillas, nombreArchivo );
+   if( cantidad > 5 ) { // para el modo
+      modo = atol( parametros[ 5 ] );
+
+      if( modo != 0 || modo != 1 ) {
+         modo = MODO;
+         printf( "Modo invalido, usando modo por defecto %d\n", modo );
+      }
+
+      printf( "Modo %d\n", modo );
+   }
+
+   printf( "Usando %d hilos para generar %ld puntos, para %ld clases -> salida: %s\n", hilos, muestras, casillas, nombreArchivo );
 
 // Procesar los parámetros del programa
 
@@ -148,24 +189,31 @@ int main( int cantidad, char ** parametros ) {
    long clases[ muestras ];		// Almacena la clase a la que pertenece cada punto
    long contClases[ casillas ];
 
-   asignarPuntosAClases( clases, 0 );	// Asigna los puntos a las clases establecidas
+   start = omp_get_wtime();
+
+   asignarPuntosAClases( clases, modo, muestras, casillas, hilos );	// Asigna los puntos a las clases establecidas
 
    do {
 	// Coloca todos los centros en el origen
 	// Promedia los elementos del conjunto para determinar el nuevo centro
 
-      actualizarCentros( centros, puntos, clases, contClases );
+      actualizarCentros( centros, puntos, clases, contClases, hilos );
 
       cambios = 0;	// Almacena la cantidad de puntos que cambiaron de conjunto
 	// Cambia la clase de cada punto al centro más cercano
 
-      actualizarPuntos( centros, puntos, clases, contClases, cambios );
+      actualizarPuntos( centros, puntos, clases, contClases, cambios, hilos );
 
       totalCambios += cambios;
 
    } while ( cambios > 0 );	// Si no hay cambios el algoritmo converge
 
+   finish = omp_get_wtime();
+   wused = finish - start;
+
    printf( "Valor de la disimilaridad en la solución encontrada %g, con un total de %ld cambios\n", centros->disimilaridad( puntos, clases ), totalCambios );
+
+   printf( "OpenMP wall time: %.6f s\n", wused );
 
 // Con los valores encontrados genera el archivo para visualizar los resultados
    puntos->genEpsFormat( centros, clases, (char *)nombreArchivo );
